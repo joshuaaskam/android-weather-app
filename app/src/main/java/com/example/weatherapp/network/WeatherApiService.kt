736b4e717a3fdcc4ccaad6ef32fb2490e9045051
joshuaaskam <115502612+joshuaaskam@users.noreply.github.com>
2024-07-10 19:56:28 -0700

    added hourly API service

--- app/src/main/java/com/example/weatherapp/network/WeatherApiService.kt
+++ app/src/main/java/com/example/weatherapp/network/WeatherApiService.kt
@@ -3,7 +3,15 @@ package com.example.weatherapp.network
 import com.example.weatherapp.model.WeeklyWeatherForecast
 import com.example.weatherapp.model.HourlyWeatherForecast
 import retrofit2.http.GET
+import retrofit2.http.Query
 interface WeatherApiService {
     @GET("v1/forecast")
-    suspend fun getHourlyWeatherForecast(): HourlyWeatherForecast
+    suspend fun getHourlyWeatherForecast(
+        @Query("latitude") latitude: Float,
+        @Query("longitude") longitude: Float,
+        @Query("hourly") hourly: String = "temperature_2m",
+        @Query("temperature_unit") temperatureUnit: String = "fahrenheit",
+        @Query("timezone") timeZone: String = "auto",
+        @Query("forecast_days") forecastDays: Int = 1
+    ): HourlyWeatherForecast
 }
