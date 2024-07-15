950966375c716363c2874a01f2b3268c276b6ae4
joshuaaskam <115502612+joshuaaskam@users.noreply.github.com>
2024-07-14 22:15:48 -0700

    Now displaying weekly forecast

--- app/src/main/java/com/example/weatherapp/ui/screens/ForecastScreen.kt
+++ app/src/main/java/com/example/weatherapp/ui/screens/ForecastScreen.kt
@@ -1,11 +1,14 @@
 package com.example.weatherapp.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
@@ -16,7 +19,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.example.weatherapp.model.Daily
 import com.example.weatherapp.model.Hourly
+import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
@@ -29,37 +34,43 @@ fun ForecastScreen(
 ) {
     when (weatherUiState) {
         is WeatherUiState.Loading -> Text(text = "Loading")
-        is WeatherUiState.Success -> Text(weatherUiState.weeklyForecast.toString())//HourlyForecastScreen(
-            //hourlyForecasts = weatherUiState.hourlyForecast.hourly, contentPadding = contentPadding
-        //)
+        is WeatherUiState.Success -> {
+            Column(
+                verticalArrangement = Arrangement.Top
+            ) {
+                HourlyForecast(
+                    hourlyForecasts = weatherUiState.hourlyForecast.hourly,
+                    contentPadding = contentPadding
+                )
+                WeeklyForecast(
+                    dailyForecasts = weatherUiState.weeklyForecast.daily,
+                    contentPadding = contentPadding
+                )
+            }
+        }
         is WeatherUiState.Error -> Text(text = weatherUiState.errorMessage) // Should add retry here
     }
 }
 
 @Composable
-fun HourlyForecastScreen(
+fun HourlyForecast(
     hourlyForecasts: Hourly,
     modifier: Modifier = Modifier,
     contentPadding: PaddingValues = PaddingValues(0.dp)
 ) {
     val tempHourPairs = hourlyForecasts.time.zip(hourlyForecasts.temperature2m)
 
-    Box (
-        modifier = Modifier.fillMaxSize().padding(8.dp),
-        contentAlignment = Alignment.TopCenter
-    ){
-        Card(
-            modifier = Modifier.wrapContentHeight(),
-            shape = MaterialTheme.shapes.medium,
-            elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
-        ) {
-            LazyRow {
-                items(tempHourPairs) {
-                    HourlyForecastCard(
-                        tempHourPair = it,
-                        modifier = modifier.padding(8.dp)
-                    )
-                }
+    Card(
+        modifier = Modifier.wrapContentHeight().padding(8.dp),
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ) {
+        LazyRow {
+            items(tempHourPairs) {
+                HourlyForecastCard(
+                    tempHourPair = it,
+                    modifier = modifier.padding(8.dp)
+                )
             }
         }
     }
@@ -73,10 +84,49 @@ fun HourlyForecastCard(tempHourPair: Pair<String, Float>, modifier: Modifier = M
     }
 }
 
+@Composable
+fun WeeklyForecast(
+    dailyForecasts: Daily,
+    modifier: Modifier = Modifier,
+    contentPadding: PaddingValues = PaddingValues(0.dp)
+) {
+    val dayMaxMinPair = dailyForecasts.time.zip(dailyForecasts.temperatureMax.zip(dailyForecasts.temperatureMin))
+
+    Card(
+        modifier = Modifier.wrapContentHeight().padding(8.dp),
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ) {
+        LazyColumn {
+            items(dayMaxMinPair) {
+                DailyForecastCard(
+                    dayMaxMinPair = it,
+                    modifier = modifier.padding(8.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun DailyForecastCard(dayMaxMinPair: Pair<String, Pair<Float, Float>>, modifier: Modifier = Modifier) {
+    Row(modifier = modifier) {
+        Text(text = formatDateToDay(dayMaxMinPair.first))
+        Text(text = dayMaxMinPair.second.first.toString() + "\u2109")
+        Text(text = dayMaxMinPair.second.second.toString() + "\u2109")
+    }
+}
+
 fun formatTimeStringToHour(input: String): String {
     val dateTime = LocalDateTime.parse(input)
 
     val outputFormatter = DateTimeFormatter.ofPattern("ha")
 
     return dateTime.format(outputFormatter)
+}
+
+fun formatDateToDay(dateString: String): String {
+    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+    val date = LocalDate.parse(dateString, formatter)
+    return date.dayOfWeek.toString()
 }
