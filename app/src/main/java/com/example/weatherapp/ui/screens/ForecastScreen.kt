8483d6f13920a944517f3a00ddd43251558890bf
joshuaaskam <115502612+joshuaaskam@users.noreply.github.com>
2025-01-04 21:23:35 -0800

    Changed order of daily forecast min and max temperature

--- app/src/main/java/com/example/weatherapp/ui/screens/ForecastScreen.kt
+++ app/src/main/java/com/example/weatherapp/ui/screens/ForecastScreen.kt
@@ -140,8 +140,8 @@ fun WeeklyForecast(
 fun DailyForecastCard(dayMaxMinPair: Pair<String, Pair<Float, Float>>, modifier: Modifier = Modifier) {
     Row(modifier = modifier) {
         Text(text = formatDateToDay(dayMaxMinPair.first) + " ")
-        Text(text = dayMaxMinPair.second.first.toString() + "\u2109" + "-")
-        Text(text = dayMaxMinPair.second.second.toString() + "\u2109")
+        Text(text = dayMaxMinPair.second.second.toString() + "\u2109" + "-")
+        Text(text = dayMaxMinPair.second.first.toString() + "\u2109")
     }
 }
 
